daebd244291679249c457527edf5c7073587336a
zuolonglong720 <2512932640@qq.com>
2019-05-13 09:20:48 +0800

    kafka

--- controllers/webhook.go
+++ controllers/webhook.go
@@ -22,19 +22,20 @@ func (this *MainController)Post(){
 	json := string(this.Ctx.Input.RequestBody)
 
     //一个文件一个结构体，为避免麻烦，就创建了个map
-    fmt.Println(json)
-	fmt.Println(gjson.Get(json, "ref"))
+   // fmt.Println(json)
+	//fmt.Println(gjson.Get(json, "ref"))
     //从前端获得数据，并解包放入resp中
   
 //	data := this.Ctx.Input.RequestBody
 //	fmt.Println(&resp)
 //	fmt.Println(string(resp["ref"]))
-	fmt.Println()
+	//fmt.Println()
 
+	fmt.Println("start kafka send")
 	syncProducer(Address,json)
 
 	this.Ctx.WriteString(json)
-	fmt.Println("这是一个webhook的测试")
+	fmt.Println("over ---")
 }
 
 
