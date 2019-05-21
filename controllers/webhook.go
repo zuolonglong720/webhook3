345cbaa797061d9bbefee303ef335782d4e28238
Z <15328141653@163.com>
2019-05-21 16:26:34 +0800

    dd

--- controllers/webhook.go
+++ controllers/webhook.go
@@ -1,47 +1,69 @@
 package controllers
 
-import (	
+import (
+	"fmt"
 	"github.com/astaxie/beego"
-//	"github.com/tidwall/gjson"
-	"github.com/Shopify/sarama"
-    "time"
-    "log"
-    "fmt"
-    "os"
- )
+	"github.com/tidwall/gjson"
+)
+
+type Param struct {
+	URL          string
+	RepositoryId string
+	Added        string
+	Removed      string
+	Modified     string
+	Commits      string
+}
+
+var Address = []string{"47.97.248.41:9092"}
+var _topic = "my-test2"
 
 type MainController struct {
 	beego.Controller
 }
 
 func (this *MainController) Get() {
-        this.Ctx.WriteString("hello")
+	this.Ctx.WriteString("hello")
 }
-func (this *MainController)Post(){
+func (this *MainController) Post() {
 
 	json := string(this.Ctx.Input.RequestBody)
 
-    //一个文件一个结构体，为避免麻烦，就创建了个map
-   // fmt.Println(json)
-	//fmt.Println(gjson.Get(json, "ref"))
-    //从前端获得数据，并解包放入resp中
-  
-//	data := this.Ctx.Input.RequestBody
-//	fmt.Println(&resp)
-//	fmt.Println(string(resp["ref"]))
+	//一个文件一个结构体，为避免麻烦，就创建了个map
+	// fmt.Println(json)
+	/*	 p := Param{}
+	commits := gjson.Get(json, "commits")
+	p.Commits = commits.String()
+
+	restoryId := gjson.Get(json, "repository.id")
+	p.RepositoryId = restoryId.String();*/
+
+	var addedResult []string
+	addeds := gjson.Get(json, "commits.#.added")
+	for _, add := range addeds.Array() {
+		fmt.Println(add)
+		for _, ad := range add.Array() {
+			fmt.Println(ad)
+			addedResult = append(addedResult, ad.String())
+		}
+	}
+	fmt.Println(addedResult)
+
+	//从前端获得数据，并解包放入resp中
+
+	//	data := this.Ctx.Input.RequestBody
+	//	fmt.Println(&resp)
+	//	fmt.Println(string(resp["ref"]))
 	//fmt.Println()
 
-	fmt.Println("start kafka send")
-	syncProducer(Address,json)
+	//fmt.Println("start kafka send")
+	//syncProducer(Address,json)
 
 	this.Ctx.WriteString("chenggong")
 	fmt.Println("over ---")
 }
 
-
-var Address = []string{"47.97.248.41:9092"}
-
-
+/*
 //同步消息模式
 func syncProducer(address []string,json string)  {
     fmt.Printf("start")
@@ -54,9 +76,7 @@ func syncProducer(address []string,json string)  {
         return
     }
     defer p.Close()
-    topic := "my-test2"
-   // srcValue := "sync: this is a zuolonglong. index=%d"
-    //for i:=0; i<10; i++ {
+    topic := _topic
         value := json
         msg := &sarama.ProducerMessage{
             Topic:topic,
@@ -69,5 +89,4 @@ func syncProducer(address []string,json string)  {
             fmt.Fprintf(os.Stdout, value + "发送成功，partition=%d, offset=%d \n", part, offset)
         }
         time.Sleep(2*time.Second)
-    //}
-}
+}*/
